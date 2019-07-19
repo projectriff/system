dcba68696ba1e5d43cb34ba3cd654da397169940
Scott Andrews <sandrews@pivotal.io>
2019-07-18 22:36:12 -0400

    Add timestamp and short gitsha to version slugs
    The new format is `{version}-{date}-{sha}`, for example
    `0.1.0-snapshot-201907190202-584c6a1d7b6e76f1`.

--- hack/release.sh
+++ hack/release.sh
@@ -6,20 +6,22 @@ set -o pipefail
 
 readonly root=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)
 readonly version=$(cat ${root}/VERSION)
-readonly branch=${1:11} # drop 'refs/head/' prefix
-readonly gitsha=$(git rev-parse HEAD)
+readonly git_branch=${1:11} # drop 'refs/head/' prefix
+readonly git_sha=$(git rev-parse HEAD)
+readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
+readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 
 echo "Building riff System"
-(cd $root && KO_DOCKER_REPO="gcr.io/projectriff/system" ko resolve -P -t "${version}" -t "${version}-${gitsha}" -f config/ | \
+(cd $root && KO_DOCKER_REPO="gcr.io/projectriff/system" ko resolve -P -t "${version}" -t "${slug}" -f config/ | \
   sed -e "s|projectriff.io/release: devel|projectriff.io/release: \"${version}\"|" > ${root}/riff-system.yaml)
 
 echo "Publishing riff System"
-gsutil cp -a public-read ${root}/riff-system.yaml gs://projectriff/riff-system/snapshots/riff-system-${version}-${gitsha}.yaml
+gsutil cp -a public-read ${root}/riff-system.yaml gs://projectriff/riff-system/snapshots/riff-system-${slug}.yaml
 gsutil cp -a public-read ${root}/riff-system.yaml gs://projectriff/riff-system/riff-system-${version}.yaml
 
 echo "Publishing version references"
-gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${version}-${gitsha}") gs://projectriff/riff-system/snapshots/versions/${branch}
-gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${version}-${gitsha}") gs://projectriff/riff-system/snapshots/versions/${version}
+gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-system/snapshots/versions/${git_branch}
+gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${slug}") gs://projectriff/riff-system/snapshots/versions/${version}
 if [[ ${version} != *"-snapshot" ]] ; then
-  gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${version}") gs://projectriff/riff-system/versions/releases/${branch}
+  gsutil -h 'Content-Type: text/plain' -h 'Cache-Control: private' cp -a public-read <(echo "${version}") gs://projectriff/riff-system/versions/releases/${git_branch}
 fi
