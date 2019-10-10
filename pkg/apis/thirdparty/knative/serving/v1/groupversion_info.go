0d554bbd4102c557b9d791f1859b4cd4fc6ba930
Scott Andrews <sandrews@pivotal.io>
2019-10-10 15:56:45 -0400

    Consume serving.knative.dev/v1 api version (#124)
    This change requires k8s 1.14+

--- pkg/apis/thirdparty/knative/serving/v1/groupversion_info.go
+++ pkg/apis/thirdparty/knative/serving/v1/groupversion_info.go
@@ -31,8 +31,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	// TODO switch version to v1 once Knative Serving exposes it
-	GroupVersion = schema.GroupVersion{Group: "serving.knative.dev", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "serving.knative.dev", Version: "v1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
