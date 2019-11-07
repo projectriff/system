962b64461c1cd392d640d199fe6cce5a03bbbe07
Scott Andrews <sandrews@pivotal.io>
2019-11-07 17:19:46 -0500

    Consume helperless FATS (#173)

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -5,8 +5,6 @@ set -o nounset
 set -o pipefail
 
 source ${FATS_DIR}/.configure.sh
-source ${FATS_DIR}/applications/helpers.sh
-source ${FATS_DIR}/functions/helpers.sh
 
 # setup namespace
 kubectl create namespace ${NAMESPACE}
@@ -14,18 +12,30 @@ fats_create_push_credentials ${NAMESPACE}
 
 for location in cluster local; do
   for test in function application; do
-    path="${test}s/uppercase/node"
     name=system-${RUNTIME}-${location}-uppercase-node-${test}
     image=$(fats_image_repo ${name})
-    if [ $location = 'cluster' ] ; then
-      create_args="--git-repo https://github.com/${FATS_REPO}.git --git-revision ${FATS_REFSPEC} --sub-path ${path}"
-    else
-      create_args="--local-path ."
-    fi
-    input_data=system
-    expected_data=SYSTEM
-    runtime=$RUNTIME
 
-    run_${test} ${FATS_DIR}/$path $name $image "$create_args" $input_data $expected_data $runtime
+    echo "##[group]Run function $name"
+
+    if [ $location = 'cluster' ] ; then
+      riff $test create $name --image $image --namespace $NAMESPACE --tail \
+        --git-repo https://github.com/${FATS_REPO}.git --git-revision ${FATS_REFSPEC} --sub-path ${test}s/uppercase/node &
+    else
+      riff $test create $name --image $image --namespace $NAMESPACE --tail \
+        --local-path ${FATS_DIR}/${test}s/uppercase/node
+    fi
+
+    riff $RUNTIME deployer create $name --${test}-ref $name --namespace $NAMESPACE --tail
+    if [ $test = 'function' ] ; then
+      source ${FATS_DIR}/macros/invoke_${RUNTIME}_deployer.sh $name "-H Content-Type:text/plain -H Accept:text/plain -d system" SYSTEM
+    else
+      source ${FATS_DIR}/macros/invoke_${RUNTIME}_deployer.sh $name "--get --data-urlencode input=system" SYSTEM
+    fi
+    riff $RUNTIME deployer delete $name --namespace $NAMESPACE
+
+    riff $test delete $name --namespace $NAMESPACE
+    fats_delete_image $image
+
+    echo "##[endgroup]"
   done
 done
